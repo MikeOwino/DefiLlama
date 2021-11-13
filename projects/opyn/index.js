8656438e2484c16b8040af2a165bd6f2beddc7ee
Mike Owino <mike.owino40@yahoo.com>
2021-11-13 18:56:27 +0300

    initial commit

---
+++ projects/opyn/index.js
@@ -0,0 +1,8 @@
+const sdk = require('@defillama/sdk')
+const v1TVL = require('./convexity');
+const v2TVL = require('./gamma');
+
+module.exports = {
+  start: 1581542700,  // 02/12/2020 @ 09:25PM (UTC)
+  tvl: sdk.util.sumChainTvls([v1TVL, v2TVL])
+}
