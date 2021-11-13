e28118b072e60e193ea43e19c93529406e12adfe
Mike Owino <mike.owino40@yahoo.com>
2021-11-13 18:27:07 +0300

    initial commit

---
+++ projects/dextf/index.js
@@ -0,0 +1,47 @@
+/*==================================================
+  Modules
+  ==================================================*/
+
+  const TVLV1 = require('./v1');
+  const TVLV2 = require('./v2');
+
+  const BigNumber = require('bignumber.js');
+
+/*==================================================
+  TVL
+  ==================================================*/
+
+  async function tvl(timestamp, block) {
+    const [v1, v2] = await Promise.all([
+      TVLV1(timestamp, block), TVLV2(timestamp, block)]);
+
+    const tokenAddresses = new Set(Object.keys(v1).concat(Object.keys(v2)));
+
+    const balances = (
+      Array
+        .from(tokenAddresses)
+        .reduce((accumulator, tokenAddress) => {
+          const v1Balance = new BigNumber(v1[tokenAddress] || '0');
+          const v2Balance = new BigNumber(v2[tokenAddress] || '0');
+
+          accumulator[tokenAddress] = v1Balance.plus(v2Balance).toFixed(0);
+
+          return accumulator
+        }, {})
+    );
+
+    return balances;
+  }
+
+/*==================================================
+  Exports
+  ==================================================*/
+
+  module.exports = {
+    name: 'dextf',
+    website: "https://dextf.com",
+    token: "DEXTF",
+    category: 'assets',
+    start: 1595853825,  // 27/07/2020 @ 12:43:45am (UTC)
+    tvl
+  }
